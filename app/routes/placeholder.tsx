ac4804de747e207fc8a71b5f4ad0703249ec42ed
daniel <danbascou@gmail.com>
2026-06-11 11:52:48 -0300

    fix: token not validated

--- app/routes/placeholder.tsx
+++ app/routes/placeholder.tsx
@@ -1,18 +1,5 @@
-import { useAuth } from "../context/AuthContext";
-import Loader from "~/components/Loader";
-import { Navigate } from "react-router";
-
 const Placeholder: React.FC = () => {
 
-  const auth = useAuth();
-
-  if (auth.checking) {
-    return <Loader />;
-  }
-
-  if (!auth.isAuthenticated) {
-    return <Navigate to="/" replace />;
-  }
   return (
     <div className="flex-1 flex items-center justify-center h-full text-[#1e1b1c]">
       <div className="text-center">
